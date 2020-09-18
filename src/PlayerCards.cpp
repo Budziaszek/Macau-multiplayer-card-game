27825ce546afa4e5788623f182747a2f93baa575
Budziaszek <budziaszek@gmail.com>
2020-09-18 21:33:03 +0200

    Moved PlayerCards class to separate files

--- src/PlayerCards.cpp
+++ src/PlayerCards.cpp
@@ -3,3 +3,73 @@
 //
 
 #include "PlayerCards.h"
+
+PlayerCards::PlayerCards() {
+    numberOfCards = 0;
+    cards = new Card[52];
+}
+
+Card PlayerCards::checkOne(int which) {
+    return cards[which];
+}
+
+void PlayerCards::show() {
+    for (int i = 0; i < numberOfCards; i++) {
+        cout << cards[i].color << " " << cards[i].figure << endl;
+    }
+}
+
+void PlayerCards::draw(Deck &deck) {
+    cards[numberOfCards] = deck.giveOne();
+    numberOfCards++;
+}
+
+void PlayerCards::draw(Card card) {
+    cards[numberOfCards] = card;
+    numberOfCards++;
+}
+
+Card PlayerCards::discard() {
+    for (int i = 0; i < numberOfCards; i++) {
+        if (cards[i].chosen == true) {
+            Card thisOne = cards[i];
+            for (; i < numberOfCards - 1; i++) {
+                cards[i] = cards[i + 1];
+            }
+            numberOfCards--;
+            cards[i].chosen = false;
+            return thisOne;
+        }
+    }
+    return Card();
+}
+
+void PlayerCards::discard(unsigned int which, Deck &deck) {
+    deck.getOne(cards[which]);
+    cards[which].chosen = false;
+    for (int i = which; i < numberOfCards - 1; i++) {
+        cards[i] = cards[i + 1];
+    }
+    numberOfCards--;
+}
+
+void PlayerCards::discard(Card card, Deck &deck) {
+    deck.getOne(card);
+    for (int i = 0; i < numberOfCards; i++) {
+        if (cards[i].color == card.color)
+            if (cards[i].figure == card.figure) {
+                cards[i].chosen = true;
+                discard();
+                return;
+            }
+    }
+}
+
+void PlayerCards::changeChoose(int which) {
+    cards[which].chosen = !cards[which].chosen;
+}
+
+void PlayerCards::resetChoosen() {
+    for (int i = 0; i < numberOfCards; i++)
+        cards[i].chosen = false;
+}
--- src/PlayerCards.h
+++ src/PlayerCards.h
@@ -5,4 +5,28 @@
 #ifndef MACAU_PLAYERCARDS_H
 #define MACAU_PLAYERCARDS_H
 
+#include "Card.h"
+#include "Deck.h"
+
+class PlayerCards
+{
+    friend class Deck;
+public:
+    PlayerCards();
+    Card checkOne(int which);
+    void changeChoose(int which);
+    void resetChoosen();
+    unsigned int numberOfCards;
+    void show();
+    void draw(Deck &deck);
+    void draw(Card card);
+    Card discard();
+    void discard(unsigned int which, Deck& deck);
+    void discard(Card card, Deck& deck);
+private:
+    Card* cards;
+
+};
+
+
 #endif //MACAU_PLAYERCARDS_H
